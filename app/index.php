b3b56fa0afc9788793eea756560b2ccbfbff0fd6
marlenetiare <marlene.jamett@gmail.com>
2015-10-28 13:08:35 -0300

    first commit

---
+++ app/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32">
+
+	<title>Power Influencer</title>
+
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+	<link rel="stylesheet" href="css/PI_landing.css">
+	<script src="js/PI_landing.min.js"></script>
+
+</head>
+<body>
+	<div class="content-home-pi">
+		<div class="menu-pi">
+			<div class="menu-center">
+				<div class="logo"></div>
+				<div class="menu-list">
+					<ul>
+						<li>Home</li>
+						<li>Plataforma</li>
+						<li>Cliente</li>
+						<li>Influencers</li>
+						<li class="ingresar-btn-menu">Ingresar</li>
+					</ul>
+				</div>	
+			</div>
+		</div>
+		<div class="content-slidr">
+			<h1>Donec vitae tristique</h1>
+			<h2>Explore and share stories from friends and the world around you.</h2>
+			<div class="btn-green">Cliente</div>
+			<div class="btn-green">Influencers</div>
+		</div>
+		<div class="demo-pi">
+			<div class="img-mobile"><img src="img/iphone.png" alt="power influencer mobile"></div>
+			<div class="nexus-mobile"><img src="img/nexus.png" alt="power influencer mobile"></div>
+		</div>
+	</div>
+</body>
+</html>
