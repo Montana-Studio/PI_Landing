75d65dac7a9c58dfa0cbc93bbfbb598d31b8fc2a
marlenetiare <marlene.jamett@gmail.com>
2015-10-28 18:24:04 -0300

    update estructura

--- app/index.php
+++ app/index.php
@@ -14,7 +14,7 @@
 
 </head>
 <body>
-	<div class="content-home-pi">
+	<section class="content-home-pi">
 		<div class="menu-pi">
 			<div class="menu-center">
 				<div class="logo"></div>
@@ -39,6 +39,159 @@
 			<div class="img-mobile"><img src="img/iphone.png" alt="power influencer mobile"></div>
 			<div class="nexus-mobile"><img src="img/nexus.png" alt="power influencer mobile"></div>
 		</div>
-	</div>
+	</section>
+	<section class="cli-section">
+		<div class="inside-section">
+			<div class="title-section">Donec vitae clientes</div>
+			<div class="title-line"></div>
+			<p>Quisque pellentesque nisi dui, et consectetur lorem facilisis eget. Nulla facilisi. Nunc nec turpis non dolor blandit pulvinar at a sem.  Quisque lacinia semper congue.</p>
+		</div>
+	</section>
+	<section class="marcas-section">
+		<div class="inside-section">
+			<div class="title-section">Donec vitae clientes</div>
+			<div class="title-line"></div>
+			<p>Quisque pellentesque nisi dui, et consectetur lorem facilisis eget. Nulla facilisi. Nunc nec turpis non dolor blandit pulvinar at a sem.  Quisque lacinia semper congue.</p>
+		</div>
+	</section>
+	<section class="influencers-section">
+		<div class="inside-section">
+			<div class="title-section">Influencers</div>
+			<div class="title-line"></div>
+			<p>Fusce tempus orci ut elit laoreet egestas. Maecenas maximus, ipsum at bibendum laoreet, felis arcu fermentum elit, id tempus massa leo ac neque. Duis dignissim dapibus bibendum.</p>
+		</div>
+	</section>
+	<section class="contacto-section">
+		<div class="inside-section">
+			<div class="text-contact">
+				<div class="title-section">Contacto</div>
+				<div class="title-line"></div> 
+				<p>Fusce tempus orci ut elit laoreet egestas. Maecenas maximus, ipsum at bibendum laoreet, felis arcu fermentum elit, id tempus massa leo ac neque. Duis dignissim dapibus bibendum.</p>
+			</div>
+			<div class="form-contact">
+				<form action="">
+					<input type="text" name="firstname" placeholder="Nombre">
+					<input type="text" name="title" placeholder="Asunto">
+					<textarea type="text" name="mesaje" placeholder="Mensaje" rows="7" cols="30"></textarea>
+					<input type="submit" value="Submit">
+				</form>
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer-pi">
+		<div class="newsletter-footer">
+			<div class="title-newsl">Suscríbete a nuestro newsletter</div>
+			<div class="form-newsletter">
+				<input type="text" name="nombre" placeholder="Nombre">
+				<input type="text" name="correo" placeholder="Correo">
+				<input class="btn-green-full" type="submit" value="Suscribir">
+			</div>
+		</div>
+		<div class="social-connect">
+			<ul>
+				<li>
+					<a href="#">
+						<div class="icon-social">
+							<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+							<g>
+								<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="29.7028" y1="31.2336" x2="21.6249" y2="17.7897">
+									<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+									<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+								</linearGradient>
+								<path fill="url(#SVGID_1_)" d="M29.049,15.749v2.851h-1.696c-0.619,0-1.037,0.13-1.253,0.389c-0.216,0.259-0.324,0.648-0.324,1.166
+									v2.041h3.164l-0.421,3.197h-2.744v8.198h-3.306v-8.198h-2.754v-3.197h2.754v-2.354c0-1.339,0.375-2.378,1.123-3.116
+									c0.749-0.738,1.746-1.107,2.993-1.107C27.644,15.62,28.465,15.663,29.049,15.749z"/>
+							</g>
+							<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="991.6994" y1="446.2881" x2="1045.5769" y2="446.2881" gradientTransform="matrix(-0.3899 -0.7181 0.6979 -0.379 110.578 925.0153)">
+								<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+								<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+							</linearGradient>
+							<path opacity="0.8" fill="none" stroke="url(#SVGID_2_)" stroke-width="1.6" stroke-miterlimit="10" d="M14.311,5.852l7.38-4.016
+								c1.837-1.016,3.62-0.992,5.441,0.191l15.401,7.944c1.633,1.052,2.33,2.705,2.354,4.478l-0.021,18.927
+								c0,2.313-0.916,4.03-3.047,5.138l-14.265,8.743c-2.203,1.246-4.231,1.088-6.317-0.264L6.354,37.349
+								c-1.634-1.068-2.49-2.585-2.476-4.597l0.073-18.328c0.01-2.001,0.943-3.445,2.641-4.375L14.311,5.852z"/>
+							</svg>
+						</div>
+						<div class="link-social">/Powerinfluencers</div>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<div class="icon-social">
+							<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+							<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="991.6382" y1="446.916" x2="1045.5156" y2="446.916" gradientTransform="matrix(-0.3899 -0.7181 0.6979 -0.379 110.578 925.0153)">
+								<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+								<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+							</linearGradient>
+							<path opacity="0.8" fill="none" stroke="url(#SVGID_1_)" stroke-width="1.6" stroke-miterlimit="10" d="M14.773,5.658l7.38-4.016
+								c1.837-1.016,3.62-0.992,5.441,0.191l15.401,7.944c1.633,1.052,2.33,2.705,2.354,4.478l-0.021,18.927
+								c0,2.313-0.916,4.03-3.047,5.138l-14.265,8.743c-2.203,1.246-4.231,1.088-6.317-0.264L6.816,37.155
+								c-1.634-1.068-2.49-2.585-2.476-4.597L4.414,14.23c0.01-2.001,0.943-3.445,2.641-4.375L14.773,5.658z"/>
+							<g>
+								<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="27.8826" y1="29.2153" x2="20.3269" y2="16.6404">
+									<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+									<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+								</linearGradient>
+								<path fill="url(#SVGID_2_)" d="M33.205,19.235c-0.475,0.693-1.047,1.284-1.719,1.772c0.006,0.099,0.011,0.248,0.011,0.446
+									c0,0.919-0.135,1.837-0.404,2.752c-0.269,0.917-0.678,1.796-1.225,2.636c-0.548,0.842-1.201,1.587-1.957,2.233
+									c-0.757,0.647-1.669,1.163-2.737,1.55c-1.069,0.385-2.211,0.578-3.427,0.578c-1.917,0-3.671-0.513-5.262-1.539
+									c0.248,0.029,0.523,0.043,0.828,0.043c1.591,0,3.009-0.488,4.253-1.464c-0.743-0.014-1.406-0.242-1.994-0.685
+									c-0.586-0.442-0.99-1.006-1.21-1.692c0.234,0.036,0.45,0.053,0.648,0.053c0.303,0,0.605-0.039,0.902-0.117
+									c-0.792-0.162-1.448-0.557-1.968-1.183c-0.52-0.626-0.78-1.352-0.78-2.18v-0.043c0.48,0.269,0.996,0.414,1.549,0.435
+									c-0.467-0.311-0.839-0.717-1.114-1.22c-0.277-0.502-0.415-1.047-0.415-1.634c0-0.622,0.156-1.198,0.467-1.729
+									c0.856,1.054,1.897,1.898,3.124,2.53c1.227,0.633,2.542,0.985,3.942,1.055c-0.057-0.268-0.085-0.53-0.085-0.785
+									c0-0.948,0.334-1.755,1.003-2.424c0.668-0.668,1.476-1.003,2.424-1.003c0.99,0,1.824,0.36,2.503,1.082
+									c0.77-0.149,1.496-0.425,2.175-0.828c-0.261,0.813-0.764,1.443-1.507,1.888C31.888,19.695,32.546,19.518,33.205,19.235z"/>
+							</g>
+							</svg>	
+						</div>
+						<div class="link-social">/Powerinfluencers</div>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<div class="icon-social">
+							<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+							<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="990.3169" y1="447.0694" x2="1044.1943" y2="447.0694" gradientTransform="matrix(-0.3899 -0.7181 0.6979 -0.379 110.578 925.0153)">
+								<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+								<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+							</linearGradient>
+							<path opacity="0.8" fill="none" stroke="url(#SVGID_1_)" stroke-width="1.6" stroke-miterlimit="10" d="M15.395,6.549l7.38-4.016
+								c1.837-1.016,3.62-0.992,5.441,0.191l15.401,7.944c1.633,1.052,2.33,2.705,2.354,4.478L45.95,34.074c0,2.313-0.916,4.03-3.047,5.138
+								l-14.265,8.743c-2.203,1.246-4.231,1.088-6.317-0.264L7.438,38.045c-1.634-1.068-2.49-2.585-2.476-4.597L5.036,15.12
+								c0.01-2.001,0.943-3.445,2.641-4.375L15.395,6.549z"/>
+							<g>
+								<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="30.4383" y1="33.5767" x2="20.4954" y2="17.029">
+									<stop  offset="0.0105" style="stop-color:#1DB89A"/>
+									<stop  offset="0.9969" style="stop-color:#1D5BC2"/>
+								</linearGradient>
+								<path fill="url(#SVGID_2_)" d="M33.027,19.682v11.241c0,0.531-0.191,0.988-0.571,1.368c-0.38,0.38-0.837,0.571-1.368,0.571H19.846
+									c-0.531,0-0.988-0.191-1.368-0.571c-0.38-0.38-0.571-0.837-0.571-1.368V19.682c0-0.532,0.191-0.988,0.571-1.368
+									c0.38-0.38,0.837-0.571,1.368-0.571h11.241c0.531,0,0.988,0.191,1.368,0.571C32.836,18.695,33.027,19.151,33.027,19.682z
+									 M31.313,30.52v-6.378h-1.328c0.131,0.413,0.197,0.843,0.197,1.289c0,0.828-0.211,1.59-0.63,2.289
+									c-0.421,0.699-0.99,1.252-1.712,1.658c-0.722,0.408-1.51,0.61-2.363,0.61c-1.293,0-2.399-0.444-3.317-1.334
+									c-0.919-0.889-1.378-1.962-1.378-3.223c0-0.446,0.066-0.876,0.197-1.289h-1.387v6.378c0,0.171,0.057,0.313,0.172,0.428
+									c0.113,0.115,0.257,0.172,0.428,0.172h10.522c0.164,0,0.306-0.057,0.423-0.172C31.254,30.833,31.313,30.691,31.313,30.52z
+									 M27.626,27.355c0.594-0.574,0.891-1.267,0.891-2.081c0-0.814-0.297-1.508-0.891-2.082c-0.593-0.574-1.311-0.861-2.15-0.861
+									c-0.833,0-1.547,0.287-2.14,0.861c-0.594,0.574-0.891,1.268-0.891,2.082c0,0.814,0.297,1.507,0.891,2.081
+									c0.593,0.574,1.308,0.861,2.14,0.861C26.316,28.216,27.034,27.929,27.626,27.355z M31.313,21.73v-1.624
+									c0-0.184-0.065-0.343-0.197-0.477c-0.131-0.135-0.291-0.202-0.482-0.202h-1.712c-0.191,0-0.35,0.067-0.482,0.202
+									c-0.131,0.134-0.197,0.294-0.197,0.477v1.624c0,0.19,0.066,0.351,0.197,0.482c0.132,0.131,0.291,0.197,0.482,0.197h1.712
+									c0.191,0,0.352-0.065,0.482-0.197C31.248,22.081,31.313,21.92,31.313,21.73z"/>
+							</g>
+							</svg>
+						</div>	
+						<div class="link-social">/Powerinfluencers</div>
+						
+					</a>
+				</li>
+			</ul>
+		</div>
+	</footer>
+
 </body>
-</html>
+</html> 
